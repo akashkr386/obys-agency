e8e47af4948bb080d96d1904019078e1973ef6d9
akash kumar <akashkumar132003@gmail.com>
2024-10-04 02:23:52 +0530

    page 2 , 3, 4, 5 completed

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function loadingAnimation(){
     })
     //page1 ka h1 
     t1.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
-        y: 120,
+        y: 140,
         stagger:0.2
     })
 }
